3595af6fb9d718f2767bc15e16a5f4499f26aad5
Olivia <oliviawolfie@pm.me>
2026-07-30 21:08:32 +0200

    Fix get-webdriver-version on arch
    Closes #29

--- scripts/get-webdriver-version.sh
+++ scripts/get-webdriver-version.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
 
+obsidian=${OBSIDIAN_BIN:-/usr/bin/obsidian}
+
+if cat $obsidian | grep -aoE '^exec electron[0-9]+ /usr/lib/obsidian/app.asar' > /dev/null;
+then
+    # Arch workaround
+    match=$(cat $obsidian | grep -aoE '^exec electron[0-9]+' | grep -aoE '[0-9]+')
+    obsidian="/usr/lib/electron$match/electron"
+fi
 # TODO: why did sed -nE 's/.*Chrome\/([0-9]+).*/\1/p' break with obsidian 1.13?
-cat ${OBSIDIAN_BIN:-/usr/bin/obsidian} | grep -aoE 'Chrome/[0-9]+.' | grep -aoE '[0-9]+' | head -n 1
+cat $obsidian | grep -aoE 'Chrome/[0-9]+.' | grep -aoE '[0-9]+' | head -n 1
+
