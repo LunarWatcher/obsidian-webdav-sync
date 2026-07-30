15194c509b8f4cd936f2c70186d23d1690123859
Olivia <oliviawolfie@pm.me>
2026-07-30 20:58:45 +0200

    Filter out the Chrome/ part

--- scripts/get-webdriver-version.sh
+++ scripts/get-webdriver-version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # TODO: why did sed -nE 's/.*Chrome\/([0-9]+).*/\1/p' break with obsidian 1.13?
-cat ${OBSIDIAN_BIN:-/usr/bin/obsidian} | grep -aoE 'Chrome/[0-9]+.' | head -n 1
+cat ${OBSIDIAN_BIN:-/usr/bin/obsidian} | grep -aoE 'Chrome/[0-9]+.' | grep -aoE '[0-9]+' | head -n 1
